130bb5285fddcb126e01ac2c7037a995be9ed1a8
Roman Boboshko <romanboboshko89@gmail.com>
2016-10-25 20:06:38 +0300

    Made search bar input as contolled input

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: ''
+        };
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(e) {
+        this.setState({
+            term: e.target.value
+        });
+    }
+
     render() {
         return (
             <form className="input-group">
-                <input />
+                <input
+                    placeholder="Get a weather in you city"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
                 <span className="input-group-btn">
                     <button
                         type="submit"
